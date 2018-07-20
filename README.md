3723f1f6ce161be838928516e97a3aa159d1b449
Jonathan Guymont <jguymont@gmail.com>
2018-07-20 16:23:51 -0400

    conversion to python 3

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # MNIST
-to download the MNIST dataset with standard train/valid/test splits, do 
+to download the MNIST dataset with standard train/valid/test (70/15/15) splits, do 
 
 ```
 python download.py --dataset=mnist --savedir={where-you-want-to-save-the-dataset}
-gunzip data/mnist.pkl.gz
 ```
--- load_mnist.py
+++ load_mnist.py
@@ -1,23 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 13:31:59 2018
+@author: Jonathan Guymont
+"""
 import pickle
+import gzip
 import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 
 def load_mnist():
     """
     load MNIST data and split into
     predefined train/dev/test
     """
-    pkl_in = open("./data/mnist.pkl.npy", "rb")
-    data = pickle.load(pkl_in)
 
-    _train_x = np.asarray(data[0][0])
-    _train_y = np.asarray(data[0][1])
-    print(_train_y)
-    print(len(_train_y))
-    _valid_x = np.asarray(data[1][0])
-    _valid_y = np.asarray(data[1][1])
-    _test_x = np.asarray(data[2][0])
-    _test_y = np.asarray(data[2][1])
+    with gzip.open('./data/mnist.pkl.gz', 'rb') as f:
+
+        _train_set, _valid_set, _test_set = pickle.load(f, encoding='latin1')
+
+        _train_x, _train_y = _train_set
+        _valid_x, _valid_y = _valid_set
+        _test_x, _test_y = _test_set
+
     return [_train_x, _train_y, _valid_x, _valid_y, _test_x, _test_y]
         
 if __name__ == '__main__':
-    load_mnist()
+    data = load_mnist()
+    _train_x = data[0]
+    plt.imshow(_train_x[0].reshape((28, 28)))
+    plt.show()
+
