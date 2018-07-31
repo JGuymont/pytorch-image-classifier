ecbd96b78e7aa966bda812ce737bf9ea3a73cd2c
Guymont <jonathan.guymont@bnc.ca>
2018-07-31 09:00:50 -0400

    dynamic structure and add dataloader

---
+++ cnn.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+"""Convnet Classifier"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from collections import OrderedDict
+
+
+# Main classifier that subclasses nn.Module
+class Classifier(nn.Module):
+    """Convnet Classifier"""
+    def __init__(self):
+        
+        self.input_size = 28
+        self.num_classes = 10
+        self.channel_sizes = [1, 10]
+        self.num_cnn_layer = len(self.channel_sizes) - 1
+        self.kernel_sizes = [10]
+        self.dense_input_size = self.compute_dense_input_size()
+
+        self.cnn_architecture = self.build_cnn()
+
+        super(Classifier, self).__init__()
+
+        self.conv = nn.Sequential(
+            #: series of cnn layers.
+            #: the cnn graph is used to learn the filters.
+            #: Filters are matrix of parameters that will 
+            #: give high value output when convolve with
+            #: image that contain certain pattern detect
+            #: by this particular filter (e.g. an edge detector)
+            self.cnn_architecture
+        )
+
+        self.dense = nn.Sequential(
+            #: fully connected network that will learn how to
+            #: classify based on the result of the convolution
+            #: with the different filters
+            nn.Linear(self.dense_input_size, self.num_classes),
+            nn.ReLU(),
+        )
+
+    def build_cnn(self):
+        """Construct the cnn architecture based
+        on provided hyperparameters"""
+        _cnn_architecture = OrderedDict()
+        for i in range(self.num_cnn_layer):
+            _cnn_architecture['conv{}'.format(i)] = nn.Conv2d(self.channel_sizes[i], self.channel_sizes[i+1], self.kernel_sizes[i])
+            _cnn_architecture['relu{}'.format(i)] = nn.ReLU()
+        return _cnn_architecture
+
+
+    def compute_dense_input_size(self, stride=1, dilation=1):
+        #: return the size of the collapsed cnn output tensor 
+        #: into one dimension. This tensor will be the feed
+        #: to the fully connected network
+        L_out = self.input_size
+        for k in self.kernel_sizes:
+            L_out = (L_out - dilation*(k - 1) - 1)/stride + 1    
+        return self.channel_sizes[-1]*L_out**2
+
+    def flatten(self, x):
+        #: Return a copy of the tensor of shape 
+        #: (batch_size, num_channels, N, N) collapsed 
+        #: into one dimension. This tensor will be the feed
+        #: to the fully connected network 
+        return x.view(-1, self.dense_input_size)     
+
+    def forward(self, x):
+        """return a vector of lenght 20 with a score
+        for each digits. The digit with the highest score
+        is the predicted one"""
+        x = self.conv(x)
+        x = self.flatten(x)
+        x = self.dense(x)
+        return F.log_softmax(x, dim=1)
+
+    def evaluate(self, dataloader):
+        """
+        evaluate accuracy
+        """
+        total = 0
+        correct = 0
+
+        for (inputs, targets) in dataloader:
+
+            _outputs = self.forward(inputs)
+            _, predicted = torch.max(_outputs.data, 1)
+            total += targets.size(0)
+            correct += predicted.eq(targets.data).cpu().sum().item()
+
+        accuracy = 100.*correct/total
+
+        return accuracy
--- load_mnist.py
+++ load_mnist.py
@@ -6,11 +6,22 @@ Created on Mon Jan 15 13:31:59 2018
 """
 import pickle
 import gzip
-import numpy as np
-import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+import random
 
-def load_mnist():
+import torch
+from torch.autograd import Variable
+
+def normalize(x):
+
+    mean = x.mean()
+    std = x.std()
+
+    normalized_x = (x - mean)/std
+
+    return normalized_x
+
+def load_mnist(train_size=1.):
     """
     load MNIST data and split into
     predefined train/dev/test
@@ -20,15 +31,59 @@ def load_mnist():
 
         _train_set, _valid_set, _test_set = pickle.load(f, encoding='latin1')
 
+        _train_size = round(train_size*len(_train_set[0]))
+        _valid_size = round(train_size*len(_valid_set[0]))
+        _test_size = len(_test_set[0])
+
         _train_x, _train_y = _train_set
-        _valid_x, _valid_y = _valid_set
-        _test_x, _test_y = _test_set
+        valid_x, valid_y = _valid_set
+        test_x, test_y = _test_set
 
-    return [_train_x, _train_y, _valid_x, _valid_y, _test_x, _test_y]
-        
+    _train_ix = range(_train_size)
+    _valid_ix = range(_valid_size)
+    _test_ix = range(_test_size)
+
+    return [normalize(_train_x)[_train_ix, :], _train_y[_train_ix], 
+            normalize(valid_x)[_valid_ix, :], valid_y[_valid_ix], 
+            normalize(test_x)[_test_ix, :], test_y[_test_ix]]
+
+def batch_sampler(x, y, batch_size=1):
+    """
+    Data loader. Combines a dataset and a sampler, and
+    provides an iterators over the training dataset.
+
+    Args:
+        batch_size (int, optional): how many samples per batch to load (default: 1).
+    """
+
+    _data_size = len(y)
+    _feature_size = x.shape[1]
+
+    _examples = range(_data_size)
+
+    dataloader = []
+
+    for _ in range(int(_data_size/batch_size)):
+
+        #: randomly select examples for current SGD iteration
+        _mini_batch = random.sample(_examples, batch_size)
+
+        #: remove current example from the list of examples
+        _examples = [example for example in _examples if example not in _mini_batch]
+
+        #: Convert torch tensor to Variable
+        _batch_x = Variable(torch.Tensor(x[_mini_batch, :])).view(batch_size, 1, 28, 28)
+        _batch_y = Variable(torch.LongTensor(y[_mini_batch]))
+
+        dataloader.append((_batch_x, _batch_y))
+
+    return dataloader
+
+
 if __name__ == '__main__':
-    data = load_mnist()
-    _train_x = data[0]
-    plt.imshow(_train_x[0].reshape((28, 28)))
-    plt.show()
+    train_x, train_y, valid_x, valid_y, test_x, test_y = load_mnist()
+    
+    TRAINLOADER = batch_sampler(train_x, train_y, batch_size=64)
 
+    # plt.imshow(train_x[0].reshape((28, 28)))
+    # plt.show()
